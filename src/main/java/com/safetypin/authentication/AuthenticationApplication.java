a81411a6f9aeffc5b6c1dc3e7e6c2c35ae503f12
KronosDP <darrel.danadyaksa19@gmail.com>
2025-02-26 13:42:53 +0700

    [REFACTOR] Remove unnecessary blank line in AuthenticationApplication

--- src/main/java/com/safetypin/authentication/AuthenticationApplication.java
+++ src/main/java/com/safetypin/authentication/AuthenticationApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AuthenticationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
