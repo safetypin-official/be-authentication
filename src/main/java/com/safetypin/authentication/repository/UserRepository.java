9e0582132978ff70e52e5950e9106e5a506e4c4a
KronosDP <darrel.danadyaksa19@gmail.com>
2025-03-06 10:26:41 +0700

    [REFACTOR] Add method to UserRepository for role-based user retrieval

--- src/main/java/com/safetypin/authentication/repository/UserRepository.java
+++ src/main/java/com/safetypin/authentication/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.safetypin.authentication.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.safetypin.authentication.model.User;
+import com.safetypin.authentication.model.Role;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     User findByEmail(String email);
+    User findByRole(Role role);
 }
