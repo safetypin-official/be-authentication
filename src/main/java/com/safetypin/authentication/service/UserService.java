94f35c9d3dbbfbf82d68342ca3c9b7b3fb72fc17
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-03-06 21:40:26 +0700

    [REFACTOR] Adhered to Camel Case

--- src/main/java/com/safetypin/authentication/service/UserService.java
+++ src/main/java/com/safetypin/authentication/service/UserService.java
@@ -15,8 +15,8 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public Optional<User> findById(UUID Id) {
-        return userRepository.findById(Id);
+    public Optional<User> findById(UUID id) {
+        return userRepository.findById(id);
     }
 
     public Optional<User> findByEmail(String email) {
