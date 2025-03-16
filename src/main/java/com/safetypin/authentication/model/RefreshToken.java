f60685eeae4eab1cfefbbfd2292e63b27326ae05
riorio805 <sefrianojieftara@gmail.com>
2025-03-16 11:49:13 +0700

    [GREEN] Create RefreshToken model and testing

---
+++ src/main/java/com/safetypin/authentication/model/RefreshToken.java
@@ -0,0 +1,26 @@
+package com.safetypin.authentication.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Transient
+    @JoinColumn(name = "user_id")
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(nullable = false)
+    private Instant expiryTime;
+}
+
