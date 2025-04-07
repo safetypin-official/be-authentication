857f55d36b155be956457369f6313b13c8afc3c6
riorio805 <sefrianojieftara@gmail.com>
2025-04-07 21:29:45 +0800

    [REFACTOR] Remove unused findByUserId method from RefreshTokenRepository and update related tests

--- src/main/java/com/safetypin/authentication/repository/RefreshTokenRepository.java
+++ src/main/java/com/safetypin/authentication/repository/RefreshTokenRepository.java
@@ -4,13 +4,10 @@ import com.safetypin.authentication.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.Instant;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     RefreshToken findByToken(String token);
-
-    Optional<RefreshToken> findByUserId(UUID userId);
 
     // maybe don't use? or call this periodically
     void deleteAllByExpiryTimeBefore(Instant expiryTime);
