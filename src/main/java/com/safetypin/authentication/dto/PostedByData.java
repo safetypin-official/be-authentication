68b0b44ccd05a2338615640e14f8790e1e66087d
Fredo <fredotanzil@gmail.com>
2025-04-24 23:47:17 +0700

    [GREEN] Implement AllArgsContructor on PostedByData

--- src/main/java/com/safetypin/authentication/dto/PostedByData.java
+++ src/main/java/com/safetypin/authentication/dto/PostedByData.java
@@ -1,5 +1,6 @@
 package com.safetypin.authentication.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class PostedByData {
     private UUID userId;
     private String name;
