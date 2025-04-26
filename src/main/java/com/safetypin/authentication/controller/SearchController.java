fe78dd333fe5f31ea41e5b39e5b44590e74301e8
Fredo <fredotanzil@gmail.com>
2025-04-26 16:52:18 +0700

    [GREEN] implement paginated user search

--- src/main/java/com/safetypin/authentication/controller/SearchController.java
+++ src/main/java/com/safetypin/authentication/controller/SearchController.java
@@ -3,12 +3,17 @@ package com.safetypin.authentication.controller;
 import com.safetypin.authentication.dto.UserResponse;
 import com.safetypin.authentication.model.User;
 import com.safetypin.authentication.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -21,16 +26,34 @@ public class SearchController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<UserResponse>> searchUsersByName(@RequestParam(required = false) String query) {
+    public ResponseEntity<Page<UserResponse>> searchUsersByName(
+            @RequestParam(required = false) String query,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
         List<User> users;
         if (query == null || query.trim().isEmpty()) {
             users = userService.findAllUsers(); // Fetch all users if no query is provided
         } else {
             users = userService.findUsersByNameContaining(query.trim());
         }
+
         List<UserResponse> userResponses = users.stream()
                 .map(User::generateUserResponse)
                 .toList(); // Use toList()
-        return ResponseEntity.ok(userResponses);
+
+
+
+
+        Pageable pageable = PageRequest.of(page, size);
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), userResponses.size());
+
+        List<UserResponse> userResponsesContent =
+                start >= userResponses.size() ? Collections.emptyList()
+                : userResponses.subList(start, end);
+
+        return ResponseEntity.ok(
+                new PageImpl<>(userResponsesContent, pageable, userResponses.size())
+        );
     }
 }
