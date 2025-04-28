0bc3ce87b018c552c6f058c4259485513ad05845
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-04-28 22:59:46 +0700

    [GREEN] Update FollowRepository to support Friend Location Tracking

--- src/main/java/com/safetypin/authentication/repository/FollowRepository.java
+++ src/main/java/com/safetypin/authentication/repository/FollowRepository.java
@@ -1,16 +1,27 @@
 package com.safetypin.authentication.repository;
 
+import com.safetypin.authentication.model.Follow;
+import com.safetypin.authentication.model.FollowId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.safetypin.authentication.model.Follow;
-
 @Repository
-public interface FollowRepository extends JpaRepository<Follow, UUID> {
+public interface FollowRepository extends JpaRepository<Follow, FollowId> {
+    
+    // Find users who the userId is following
+    @Query("SELECT f.followingId FROM Follow f WHERE f.followerId = :userId")
+    List<UUID> findFollowingIds(@Param("userId") UUID userId);
+    
+    // Find users who follow the userId
+    @Query("SELECT f.followerId FROM Follow f WHERE f.followingId = :userId")
+    List<UUID> findFollowerIds(@Param("userId") UUID userId);
+
     List<Follow> findByFollowerId(UUID followerId);
 
     List<Follow> findByFollowingId(UUID followingId);
