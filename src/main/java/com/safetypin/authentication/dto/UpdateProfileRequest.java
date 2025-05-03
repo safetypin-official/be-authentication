af5594112cc49367d43972a7602e4eb9f88284ca
KronosDP <darrel.danadyaksa19@gmail.com>
2025-05-03 13:08:44 +0700

    [Refactor] Update ProfileService and UpdateProfileRequest to streamline profile updates and improve code organization

--- src/main/java/com/safetypin/authentication/dto/UpdateProfileRequest.java
+++ src/main/java/com/safetypin/authentication/dto/UpdateProfileRequest.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UpdateProfileRequest {
+    private String name;
     private String instagram;
     private String twitter;
     private String line;
